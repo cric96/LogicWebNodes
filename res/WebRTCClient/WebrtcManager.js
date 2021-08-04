9f2d21805796204d6c75b95274e16b96e9f34d6e
Denys <denys.grushchak@studio.unibo.it>
2021-08-04 11:02:22 +0200

    OOP refactoring

--- res/WebRTCClient/WebrtcManager.js
+++ res/WebRTCClient/WebrtcManager.js
@@ -1,16 +1,79 @@
 class WebrtcManager{
 
-	constructor(){
-
+	addConnection(){
+		this.peer = new ConnectedPeer(); 
+		this.peer.newChannel();
 	}
 
-	print(){console.log("method works") ;}
+	sendToAll(msg){
+		this.peer.channel.send(msg);
+	}
 
+	async elaborateMsg(json){
+		if(json.desc){
+			if (json.desc.type === 'offer') {
+				this.peer = new ConnectedPeer();
+				await this.peer.peerConnection.setRemoteDescription(json.desc);
+				await this.peer.peerConnection.setLocalDescription(await this.peer.peerConnection.createAnswer());
+				webSocket.send(JSON.stringify({desc: this.peer.peerConnection.localDescription}));
+			} else if (json.desc.type === 'answer') {
+				await this.peer.peerConnection.setRemoteDescription(json.desc);
+			} else {
+				console.log('Unsupported SDP type.');
+			}
 
+		}else if(json.candidate){
+			console.log("received candidate: ", json);
+			await this.peer.peerConnection.addIceCandidate(json);
+		}else{
+			console.log("the received msg is not recognized: " + data);
+		}
+	}
 }
 
 class ConnectedPeer{
+
 	constructor(){
-		
+		this.peerConnection =  new RTCPeerConnection();
+		this.peerConnection.ondatachannel = (event) =>  this.#setUpChannel(event.channel);
+		this.peerConnection.oniceconnectionstatechange = (e) => {
+			if(this.peerConnection.iceConnectionState == 'disconnected'){
+				console.log("disconnected");
+			}
+		};
+	}
+	
+	newChannel(){
+		this.#setUpChannel(this.peerConnection.createDataChannel("Channel"));
+
+		// Send any ice candidates to the other peer.
+		this.peerConnection.onicecandidate = e => {if(e.candidate != null) webSocket.send(JSON.stringify(e.candidate))}
+
+		// Let the "negotiationneeded" event trigger offer generation.
+		this.peerConnection.onnegotiationneeded = async () => {
+			try {
+				await this.peerConnection.setLocalDescription(await this.peerConnection.createOffer());
+				// Send the offer to the other peer.
+				let obj = {desc: this.peerConnection.localDescription};
+				webSocket.send(JSON.stringify(obj));
+			} catch (err) {
+				console.error(err);
+			}
+		};
+	}
+
+	#setUpChannel(newChannel) {
+		this.channel = newChannel;
+		this.channel.onopen = () => console.log("Open a new P2P connection");//handleChannelOpen;
+		this.channel.onmessage = handleReceiveMessage;
+		this.channel.onclose = () => console.log("Close a P2P connection"); //handleChannelClose;
+	}
+
+}
+
+function handleReceiveMessage(m){
+	console.log("M.data.msg:", JSON.parse(m.data).msg);
+	if(JSON.parse(m.data).msg === "ackInc"){
+		label.innerHTML = ++counter;
 	}
 }
--- res/WebRTCClient/script.js
+++ res/WebRTCClient/script.js
@@ -8,8 +8,10 @@ let webSocket;
 let connection;
 let channel;
 
+let connectionsManager;
+
 window.onload = function(){
-	man = new WebrtcManager();
+	connectionsManager = new WebrtcManager();
 	setUpDOM();
 	setUpWebSocket();
 }
@@ -22,8 +24,8 @@ function setUpDOM() {
     // Set event listeners for user interface widgets
 
    	document.getElementById("inc").onclick = () => webSocket.send('inc');
-	document.getElementById("incAll").onclick = () => channel?.send('{"msg": "ackInc"}');
-	document.getElementById("connect").onclick = () =>  connectPeer();
+	document.getElementById("incAll").onclick = () => connectionsManager.sendToAll('{"msg": "ackInc"}');
+	document.getElementById("connect").onclick = () =>  connectionsManager.addConnection();;
   }
 
 
@@ -42,25 +44,9 @@ function setUpWebSocket(){
 				if(json.msg === "ackInc"){
 					label.innerHTML = ++counter;
 				}
-			}else if(json.desc){
-				if (json.desc.type === 'offer') {
-					connection = createConnection();
-					await connection.setRemoteDescription(json.desc);
-					await connection.setLocalDescription(await connection.createAnswer());
-					webSocket.send(JSON.stringify({desc: connection.localDescription}));
-				} else if (json.desc.type === 'answer') {
-					await connection.setRemoteDescription(json.desc);
-				} else {
-					console.log('Unsupported SDP type.');
-				}
-
-			}else if(json.candidate){
-				console.log("received candidate: ", json);
-				await connection.addIceCandidate(json);
 			}else{
-				console.log("the received msg is not recognized: " + data);
-			}
-
+				connectionsManager.elaborateMsg(json);
+			} 
 		}catch(err){
 			console.error(err);
 		}
@@ -74,70 +60,4 @@ function setUpWebSocket(){
 	webSocket.onerror = function(error) {
 		console.log('error', error.message);
 	};
-}
-
-
-function connectPeer(){
-
-	connection = createConnection();
-	
-	
-	channel = connection.createDataChannel("Channel");
-    channel.onopen = () => console.log("Opend p2p");//handleChannelOpen;
-	channel.onmessage = handleReceiveMessage;
-	channel.onclose = () => console.log("close p2p"); //handleChannelClose;
-
-	// Send any ice candidates to the other peer.
-	connection.onicecandidate = e => {if(e.candidate != null) webSocket.send(JSON.stringify(e.candidate))}
-
-	// Let the "negotiationneeded" event trigger offer generation.
-	connection.onnegotiationneeded = async () => {
-		try {
-			await connection.setLocalDescription(await connection.createOffer());
-			// Send the offer to the other peer.
-			obj = {desc: connection.localDescription};
-			webSocket.send(JSON.stringify(obj));
-		} catch (err) {
-			console.error(err);
-		}
-	};
-
-	// Once remote data arrives
-	connection.ontrack = (event) => {
-		label.innerHTML = event.msg;
-	};
-}
-
-function createConnection(){
-	let conn = new RTCPeerConnection();
-	conn.ondatachannel = receiveChannelCallback;
-	conn.oniceconnectionstatechange = handleReceiveChannelStatusChange;
-	return conn; 
-}
-
- function receiveChannelCallback(event) {
-    channel = event.channel;
-    channel.onopen = () => console.log("Opend p2p");//handleChannelOpen;
-	channel.onmessage = handleReceiveMessage;
-	channel.onclose = () => console.log("close p2p"); //handleChannelClose;
-  }
-
-
-// Handle an error that occurs during addition of ICE candidate.
-function handleAddCandidateError() {
-	console.log("Oh noes! addICECandidate failed!");
-}
-
-function handleReceiveMessage(m){
-	console.log("M.data.msg:", JSON.parse(m.data).msg);
-	if(JSON.parse(m.data).msg === "ackInc"){
-		label.innerHTML = ++counter;
-	}
-	//console.log("msg p2p:", m);// handleChannelOnMessage;
-}
-
-function handleReceiveChannelStatusChange(event){
-	if(connection.iceConnectionState == 'disconnected'){
-			console.log("disconnected");
-	}
 }
