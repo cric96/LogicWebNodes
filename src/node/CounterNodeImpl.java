783af47d7cca9e0c933d1ecba7f1210506a5e7e2
Denys <denys.grushchak@studio.unibo.it>
2021-08-09 14:41:33 +0200

    add node id

--- src/node/CounterNodeImpl.java
+++ src/node/CounterNodeImpl.java
@@ -3,16 +3,28 @@ package node;
 import org.json.JSONObject;
 
 public class CounterNodeImpl implements CounterNode {
-	private int counter = 0;
+	private final int id;
+	volatile private int counter = 0;
+	
+	public CounterNodeImpl(int id) {
+		this.id = id;
+	}
 
 	@Override
 	public JSONObject getState() {
-		return new JSONObject().put("counter", counter);
+		return new JSONObject()
+				.put("id", id)
+				.put("counter", counter);
 	}
 
 	@Override
 	public void incrementCounter() {
 		this.counter++;
+	}
+
+	@Override
+	public int getId() {
+		return this.id;
 	}
 	
 }
--- src/node/Node.java
+++ src/node/Node.java
@@ -8,5 +8,7 @@ public interface Node {
 	 * Return a json that contains a state of node
 	 */
 	JSONObject getState();
+	
+	int getId();
 
 }
